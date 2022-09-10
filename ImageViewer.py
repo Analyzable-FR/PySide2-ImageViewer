686a57bf1bafb47d58e81f5cf7ffebe7202dd917
bgallois <benjamin@gallois.cc>
2022-09-10 10:43:23 +0200

    [feat] replace QLabel by QGraphicsScene for performance

--- ImageViewer.py
+++ ImageViewer.py
@@ -23,39 +23,36 @@ SOFTWARE.
 '''
 
 
-from PySide2.QtWidgets import QGridLayout, QApplication, QWidget, QScrollArea, QAction, QLabel
+from PySide2.QtWidgets import QOpenGLWidget, QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QApplication, QWidget, QScrollArea, QAction, QLabel
 from PySide2.QtCore import QEvent, Signal, Slot, Qt, QPoint
 from PySide2.QtGui import QImage, QPixmap, QFont, QPainter, QPen, QCursor
 import rc_resources
 
 
 
-class ImageViewer(QWidget):
+class ImageViewer(QGraphicsView):
 
     def __init__(self):
         super().__init__()
 
         self.setMouseTracking(True);
+        self.setViewport(QOpenGLWidget())
 
-        self.view = QLabel(self)
-        self.view.installEventFilter(self)
-        self.view.setScaledContents(True)
+        self.scene = QGraphicsScene(self)
+        self.image = QGraphicsPixmapItem()
+        self.scene.addItem(self.image)
+        self.setScene(self.scene)
+        self.setTransformationAnchor(QGraphicsView.AnchorViewCenter)
+        self.setDragMode(QGraphicsView.NoDrag)
 
-        self.scrollArea = QScrollArea(self)
-        self.scrollArea.setWidget(self.view)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.scrollArea.viewport().installEventFilter(self)
         self.currentZoom = 1
-
-        self.layout = QGridLayout(self)
-        self.layout.addWidget(self.scrollArea)
-        self.setLayout(self.layout)
 
         self.pixmap = None
         self.isDrawable = True
         self.brushPixmap = QPixmap(":/assets/cursor.png")
         self.setBrush()
+
+        self.painter = QPainter()
 
     def setBrush(self, color=Qt.white, size=25, factor=1):
         '''
@@ -70,72 +67,63 @@ class ImageViewer(QWidget):
         Open an image from a file.
         '''
         self.pixmap = QPixmap(path)
-        self.view.setPixmap(self.pixmap)
+        self.painter = QPainter(self.pixmap)
+        self.image.setPixmap(self.pixmap)
 
-    def eventFilter(self, source, event):
-        if event.type() == QEvent.MouseButtonPress:
-            mouseEvent = event
-            if source is self.scrollArea.viewport():
-                if mouseEvent.buttons() == Qt.MiddleButton: # Get pan coordinates reference with middle click
-                    QApplication.setOverrideCursor(Qt.ClosedHandCursor)
-                    self.panReferenceClick = mouseEvent.pos()
-                elif mouseEvent.buttons() == Qt.LeftButton and mouseEvent.modifiers() == Qt.ControlModifier: # Get coordinates reference for brush size
-                    self.brushReference = mouseEvent.pos() # Cursor will be drawed below. TO DO need refactoring.
-            if source is self.view:
-                if mouseEvent.buttons() == Qt.LeftButton: # Get drawing coordinates reference with left click
-                    QApplication.setOverrideCursor(self.drawingCursor)
-                    self.drawReference = QPoint((event.pos().x()*self.view.pixmap().size().width())/self.view.size().width(), (event.pos().y()*self.view.pixmap().size().height())/self.view.size().height())
-            return False
+    def wheelEvent(self, event):
+        '''
+        Zoom with wheel.
+        '''
+        if event.angleDelta().y() > 0:
+            factor = 1.25
+            self.currentZoom *= factor
+            self.setBrush(size=self.brushSize, factor=self.currentZoom)
+        else:
+            factor = 0.8
+            self.currentZoom *= factor
+            self.setBrush(size=self.brushSize, factor=self.currentZoom)
+        self.scale(factor, factor)
 
-        if event.type() == QEvent.MouseMove:
-            moveEvent = event
-            if source is self.scrollArea.viewport():
-                if moveEvent.buttons() == Qt.MiddleButton: # pan with middle click pressed
-                    self.scrollArea.horizontalScrollBar().setValue(self.scrollArea.horizontalScrollBar().value() + (self.panReferenceClick.x() - moveEvent.pos().x()))
-                    self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().value() + (self.panReferenceClick.y() - moveEvent.pos().y()))
-                    self.panReferenceClick = moveEvent.pos();
-                elif moveEvent.buttons() == Qt.LeftButton and moveEvent.modifiers() == Qt.ControlModifier:
-                    if moveEvent.pos().x() - self.brushReference.x() > 25:
-                        self.brushSize += 5
-                        self.setBrush(size=self.brushSize, factor=self.currentZoom)
-                        self.cursor().setPos(self.mapToGlobal(self.brushReference))
-                        QApplication.restoreOverrideCursor();
-                        QApplication.setOverrideCursor(self.drawingCursor)
-                    elif moveEvent.pos().x() - self.brushReference.x() <  -25:
-                        self.brushSize -= 5
-                        self.setBrush(size=max(self.brushSize, 5), factor=self.currentZoom)
-                        self.cursor().setPos(self.mapToGlobal(self.brushReference))
-                        QApplication.restoreOverrideCursor();
-                        QApplication.setOverrideCursor(self.drawingCursor)
+    def mousePressEvent(self, event):
+        '''
+        Pan with middle click, draw with left, change brush size with CTRL + left click + horizontal drag.
+        '''
+        if event.buttons() == Qt.MiddleButton: # Get pan coordinates reference with middle click
+            QApplication.setOverrideCursor(Qt.ClosedHandCursor)
+            self.panReferenceClick = event.pos()
+        elif event.buttons() == Qt.LeftButton and event.modifiers() == Qt.ControlModifier: # Get coordinates reference for brush size
+            self.brushReference = event.pos() # Cursor will be drawed below. TO DO need refactoring.
+        if event.buttons() == Qt.LeftButton: # Get drawing coordinates reference with left click
+            QApplication.setOverrideCursor(self.drawingCursor)
+            self.drawReference = self.mapToScene(event.pos())
 
-            elif source is self.view:
-                if moveEvent.buttons() == Qt.LeftButton and self.isDrawable and moveEvent.modifiers() == Qt.NoModifier: # Draw with left click pressed
-                    painter = QPainter(self.pixmap)
-                    painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap))
-                    painter.drawLine(self.drawReference, QPoint((event.pos().x()*self.view.pixmap().size().width())/self.view.size().width(), (event.pos().y()*self.view.pixmap().size().height())/self.view.size().height()))
-                    self.drawReference = QPoint((event.pos().x()*self.view.pixmap().size().width())/self.view.size().width(), (event.pos().y()*self.view.pixmap().size().height())/self.view.size().height())
-                    self.view.setPixmap(self.pixmap)
-            return False
+    def mouseReleaseEvent(self, event):
+        QApplication.restoreOverrideCursor();
 
-        if event.type() == QEvent.MouseButtonRelease: # Reset default cursor
-            QApplication.restoreOverrideCursor();
-            return True
-
-        if event.type() == QEvent.Wheel and source is self.scrollArea.viewport(): # Zoom with mouse wheel
-            wheelEvent = event
-            if wheelEvent.angleDelta().y() > 0:
-                factor = 1.25
-                self.currentZoom *= factor
+    def mouseMoveEvent(self, event):
+        '''
+        Pan with middle click, draw with left, change brush size with CTRL + left click + horizontal drag.
+        '''
+        if event.buttons() == Qt.MiddleButton: # pan with middle click pressed
+            self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() + (self.panReferenceClick.x() - event.pos().x()))
+            self.verticalScrollBar().setValue(self.verticalScrollBar().value() + (self.panReferenceClick.y() - event.pos().y()))
+            self.panReferenceClick = event.pos();
+        elif event.buttons() == Qt.LeftButton and event.modifiers() == Qt.ControlModifier:
+            if event.pos().x() - self.brushReference.x() > 25:
+                self.brushSize += 5
                 self.setBrush(size=self.brushSize, factor=self.currentZoom)
-                self.view.setFixedSize(factor*self.view.size())
-                self.scrollArea.horizontalScrollBar().setValue(factor*self.scrollArea.horizontalScrollBar().value() + ((factor-1)*self.scrollArea.horizontalScrollBar().pageStep()/2))
-                self.scrollArea.verticalScrollBar().setValue(factor*self.scrollArea.verticalScrollBar().value() + ((factor-1)*self.scrollArea.verticalScrollBar().pageStep()/2))
-            else:
-                factor = 0.8
-                self.currentZoom *= factor
-                self.setBrush(size=self.brushSize, factor=self.currentZoom)
-                self.view.setFixedSize(factor*self.view.size())
-                self.scrollArea.horizontalScrollBar().setValue(factor*self.scrollArea.horizontalScrollBar().value() + ((factor-1)*self.scrollArea.horizontalScrollBar().pageStep()/2))
-                self.scrollArea.verticalScrollBar().setValue(factor*self.scrollArea.verticalScrollBar().value() + ((factor-1)*self.scrollArea.verticalScrollBar().pageStep()/2))
-            return True
-        return super(ImageViewer, self).eventFilter(source, event)
+                self.cursor().setPos(self.mapToGlobal(self.brushReference))
+                QApplication.restoreOverrideCursor();
+                QApplication.setOverrideCursor(self.drawingCursor)
+            elif event.pos().x() - self.brushReference.x() <  -25:
+                self.brushSize -= 5
+                self.setBrush(size=max(self.brushSize, 5), factor=self.currentZoom)
+                self.cursor().setPos(self.mapToGlobal(self.brushReference))
+                QApplication.restoreOverrideCursor();
+                QApplication.setOverrideCursor(self.drawingCursor)
+
+        if event.buttons() == Qt.LeftButton and self.isDrawable and event.modifiers() == Qt.NoModifier: # Draw with left click pressed
+            self.painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap))
+            self.painter.drawLine(self.drawReference, self.mapToScene(event.pos()))
+            self.drawReference = self.mapToScene(event.pos())
+            self.image.setPixmap(self.pixmap)
